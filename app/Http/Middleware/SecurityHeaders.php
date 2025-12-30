3a714c58a27bbc8b8b6ff2ea0300b945e2b066ab
jejak-awan <jejakawan007@gmail.com>
2025-12-30 16:07:02 +0700

    fix: add another CSP script hash for inline scripts

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -60,6 +60,7 @@ class SecurityHeaders
             "'sha256-NrYVPfGWcGFTIEFXOF0FJ30yVDzC8vN6pXoAMPaU5RI='", // SiteConfig script (line 24)
             "'sha256-EuHBk5xLTkeNQNYrCUayLQWeV5z2hbGuaMU/F+w38hk='", // New script (likely error page or Vite)
             "'sha256-2s8oON1aK5LMVmmlBFeRS/prDWzv6u88194WZ9kAlzU='", // Additional inline script
+            "'sha256-sDrJRntOWyq4dfnjaDx4A+deE33GOSfLPqcl465G7sM='", // Latest inline script fix
         ]);
         
         // Add same-origin domains
