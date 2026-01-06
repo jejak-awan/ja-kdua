68d2ea31b6afb974c4a560c6c089be964a4c8fb6
jejak-awan <jejakawan007@gmail.com>
2026-01-06 20:39:36 +0700

    style(builder): Fix Sidebar Back to Top overlapping footer
    - Moved Sidebar button up to bottom-20 (80px) to clear Keyboard Shortcuts footer
    - Kept Properties Panel button at bottom-8 (32px) as it has no fixed footer

--- resources/js/components/builder/sidebar/Sidebar.vue
+++ resources/js/components/builder/sidebar/Sidebar.vue
@@ -178,7 +178,7 @@
         
         <BackToTop 
             :show="showBackToTop && builder.isSidebarOpen.value" 
-            positionClass="bottom-8 left-1/2 -translate-x-1/2"
+            positionClass="bottom-20 left-1/2 -translate-x-1/2"
             @click="scrollToTop" 
         />
     </div>
